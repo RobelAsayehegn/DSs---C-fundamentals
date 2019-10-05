564434a672bd631bc2f1a7412bdafc5e77f94af1
RobelAsayehegn <56114349+RobelAsayehegn@users.noreply.github.com>
2019-10-05 01:27:30 -0700

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+    int i, ceillingNumber, t1 = 0, t2 = 1, nextTerm;
+    
+    int fibonaccibiginning[] = {0,1};
+    
+    printf("Enter the ceilling number: ");
+    scanf("%d", &ceillingNumber);
+
+    printf("Fibonacci Series: ");
+    Display(ceillingNumber,fibonaccibiginning);
+    
+    return 0;
+}
+
+void Display(int ceillingNumber,int fibonaccibiginning[]){
+    int i = 0,sum = 0;
+    if (fibonaccibiginning[1] <= ceillingNumber)
+    {
+        for (i; fibonaccibiginning[0] <= ceillingNumber; ++i){
+            printf("%d, ", fibonaccibiginning[0]);
+            sum = fibonaccibiginning[0] + fibonaccibiginning[1];
+            fibonaccibiginning[0] = fibonaccibiginning[1];
+            fibonaccibiginning[1] = sum;
+        }    
+    }
+    
+}
