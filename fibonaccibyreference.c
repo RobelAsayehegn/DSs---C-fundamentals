31dfe912f5381a4852b8331477f3d2e56b3c7a63
RobelAsayehegn <56114349+RobelAsayehegn@users.noreply.github.com>
2019-10-07 01:35:05 -0700

    Fibonacci number passing by reference

---
+++ fibonaccibyreference.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+void Display(int* x);
+int main() {
+    int i, ceillingNumber;
+    
+    printf("Enter the ceilling number: ");
+    scanf("%d", &ceillingNumber);
+
+    printf("Fibonacci Series: ");
+    Display(&ceillingNumber);
+    
+    return 0;
+}
+
+void Display(int* ceillingNum){
+    int i = 0,sum = 0;
+    int fibonaccibiginning[] = {0,1};
+    int ceillingNumber = *ceillingNum;
+    if (fibonaccibiginning[1] <= ceillingNumber)
+    {
+        for (i; fibonaccibiginning[0] <= ceillingNumber; ++i){
+            printf("%d, ", fibonaccibiginning[0]);
+            sum = fibonaccibiginning[0] + fibonaccibiginning[1];
+            fibonaccibiginning[0] = fibonaccibiginning[1];
+            fibonaccibiginning[1] = sum;
+        }    
+    }
+    
+}
